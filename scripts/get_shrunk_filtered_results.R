cdb6aba2a03015be8720958f36202f139c23fe67
lieker <l.e.vlaar@uva.nl>
2021-09-07 13:52:27 +0200

    made figures for dauer paper

--- scripts/get_shrunk_filtered_results.R
+++ scripts/get_shrunk_filtered_results.R
@@ -17,7 +17,8 @@ get_shrunk_filtered_results <- function(d = dds,
   res_shr <- lfcShrink(dds = d,
                        res = res,
                        type = "ashr",
-                       contrast = c("group", target[2], target[1]))
+                       contrast = c("group", target[2], target[1])) %>% as.data.frame()
+  res_shr$padj[is.na(res_shr$padj)] <- 0.99
   res_shr_filtered <- res_shr %>% as.data.frame() %>% dplyr::filter(., padj < padj_threshold) %>% 
     dplyr::filter(., log2FoldChange > log2FC_threshold | log2FoldChange < -log2FC_threshold)
   return(res_shr_filtered)
