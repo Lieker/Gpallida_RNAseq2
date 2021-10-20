5248cab9eca6fdfebf20327849b7d09890948cb9
lieker <l.e.vlaar@uva.nl>
2021-10-20 11:39:36 +0200

    update with Waterston single gene plots

--- scripts/get_DESeq_dds.R
+++ scripts/get_DESeq_dds.R
@@ -11,8 +11,28 @@ get_DESeq_dds <- function(counts_csv_file = "input/counts.csv",
                      fileEncoding = "UTF-8-BOM") %>% column_to_rownames("Geneid")
   counts <- as.data.frame(t(counts)) %>% rownames_to_column("sample")
   
-  xp_design <- read.csv(file = xp_design_csv_file, header = TRUE, check.names = FALSE, fileEncoding = "UTF-8-BOM") 
+  xp_design <<- read.csv(file = xp_design_csv_file, header = TRUE, check.names = FALSE, fileEncoding = "UTF-8-BOM") 
   xp_design$tp <- as.factor(xp_design$tp)
+  
+  counts <- counts %>% column_to_rownames("sample") %>% t()
+  dds <- DESeqDataSetFromMatrix(countData = counts, colData = xp_design, design = ~ group)
+  
+  keep <- rowSums(counts(dds)) >= 10
+  dds <- dds[keep,]
+  dds <- DESeq(dds)
+  return(dds)
+}
+
+get_DESeq_dds2 <- function(counts_csv_file = "input/countsW.csv",
+                          xp_design_csv_file = "input/xp_designW.csv"
+) {
+  counts <- read.csv(file = counts_csv_file, 
+                     header = TRUE, 
+                     stringsAsFactors = FALSE, 
+                     fileEncoding = "UTF-8-BOM") %>% column_to_rownames("Geneid")
+  counts <- as.data.frame(t(counts)) %>% rownames_to_column("sample")
+  
+  xp_design <- read.csv(file = xp_design_csv_file, header = TRUE, check.names = FALSE, fileEncoding = "UTF-8-BOM") 
   
   counts <- counts %>% column_to_rownames("sample") %>% t()
   dds <- DESeqDataSetFromMatrix(countData = counts, colData = xp_design, design = ~ group)
--- scripts/plotcounts2.R
+++ scripts/plotcounts2.R
@@ -29,3 +29,33 @@ plotcounts2 <- function(gene, d = dds) {
   
   return(g)
 }
+
+plotcounts3 <- function(gene, d = dds, s = c("L2","dauerentry","dauer","dauerexit","L3")) {
+  data <- plotCounts(d, gene,
+                     intgroup = "group", 
+                     returnData = TRUE) %>% rownames_to_column("sample")
+  data$group <- factor(data$group, levels = c("L1", "L2", "dauerentry","dauer","dauerexit","L3","L4male","L4","L4soma","YA"), ordered = TRUE)
+  data <- data[order(data$group), ]
+  colnames(data)[2] <- "counts"
+  
+  data.summary <- data %>% group_by(group) %>% summarise(sd = sd(counts, na.rm = TRUE),
+                                                                   counts = mean(counts))
+  
+  sel <- s
+  data <- data[data$group %in% sel,]
+  data.summary <- data.summary[data.summary$group %in% sel,]
+  
+  g <- ggplot(data, aes(x=group, y=counts)) + 
+    geom_jitter(position = position_jitter(0.2),
+                alpha = 0.8) +
+    geom_line(data = data.summary,
+              aes(group = 1),
+              colour = "red3") +
+    geom_point(data = data.summary, size = 3, colour = "red3") +
+    theme_classic() +
+    ggtitle(paste0(gene," expression levels")) +
+    scale_y_log10(breaks=c(1,2,5,10,20,50,100,200,500,1000,2000,5000,10000,20000,50000)) +
+    theme(axis.text.x = element_text(angle = 45, hjust=1))
+  
+  return(g)
+}
